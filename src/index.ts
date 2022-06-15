b3a5ae378d674807385e4e36fe6dd18bd1784bd2
lenconda <i@lenconda.top>
2022-06-15 22:44:00 +0800

    feat: 🎸 initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import * as inquirer from 'inquirer';
+
+inquirer.prompt([
+    {
+        name: 'name',
+        message: 'Name for this package',
+    },
+    {
+        name: 'version',
+        message: 'Version for this package',
+        default: '0.1.0',
+    },
+    {
+        name: 'description',
+        message: 'Description for this package',
+        default: '',
+    },
+]).then((data = {}) => {
+    
+});
