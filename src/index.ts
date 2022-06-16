9105c48c22dfab448495ae6bb1eb3f32609ceebb
lenconda <i@lenconda.top>
2022-06-16 09:48:23 +0800

    feat: 🎸 write file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import * as inquirer from 'inquirer';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+import _ from 'lodash';
 
 inquirer.prompt([
     {
@@ -16,6 +19,61 @@ inquirer.prompt([
         default: '',
     },
 ]).then((data = {}) => {
-    console.log(process.cwd());
-    console.log(process.argv);
+    const PROCESS_CWD = process.cwd();
+    const TARGET_FOLDER = process.argv[2] || '';
+    const TEMPLATE_DIR = path.resolve(__dirname, '../template');
+    const TARGET_DIR = path.resolve(PROCESS_CWD, TARGET_FOLDER);
+    const packageConfig = {};
+
+    try {
+        _.merge(
+            packageConfig,
+            fs.readJsonSync(path.resolve(TEMPLATE_DIR, 'package.json._')),
+            data,
+        );
+    } catch (e) {
+        console.log('Failed to read content of default package.json');
+        process.exit(1);
+    }
+
+    if (fs.existsSync(TARGET_DIR) && !fs.statSync(TARGET_DIR).isDirectory()) {
+        console.log('There is already a file at ' + TARGET_DIR + ', please remove it and retry');
+        process.exit(1);
+    }
+
+    if (!fs.existsSync(TARGET_DIR)) {
+        fs.mkdirpSync(TARGET_DIR);
+    }
+
+    try {
+        fs.writeFileSync(
+            path.resolve(TARGET_DIR, 'package.json'),
+            JSON.stringify(packageConfig, null, 4),
+            {
+                encoding: 'utf-8',
+            },
+        );
+    } catch (e) {
+        console.log('Failed to write file package.json');
+        process.exit(1);
+    }
+
+    [
+        'tsconfig.json._',
+        'tsconfig.package.json._',
+    ].forEach((fileName) => {
+        try {
+            fs.writeFileSync(
+                path.resolve(TARGET_DIR, fileName.slice(0, -2)),
+                fs.readFileSync(path.resolve(TEMPLATE_DIR, fileName)).toString(),
+                {
+                    encoding: 'utf-8',
+                },
+            );
+        } catch (e) {
+            console.log('Failed to write file', fileName);
+        }
+    });
+
+    console.log('Project initialized at ', TARGET_DIR);
 });
